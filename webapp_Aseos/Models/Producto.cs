02f45982ac59161492056d7e9b7cbca8b6083a61
= <=>
2024-06-20 14:15:35 -0700

    'Producto' class added

---
+++ webapp_Aseos/Models/Producto.cs
@@ -0,0 +1,11 @@
+namespace webapp_Aseos.Models
+{
+    public class Producto
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+    }  
+}
