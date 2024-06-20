73b27c8ecae0e0d6391d8abdcbe01ed81e0c9cb4
= <=>
2024-06-20 14:24:24 -0700

    AppDBContext, controllers and endpoints added to Program.cs

--- webapp_Aseos/Program.cs
+++ webapp_Aseos/Program.cs
@@ -1,14 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using webapp_Aseos.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Get the configuration from the app settings.
+var configuration = builder.Configuration;
+
+// Set up the DB context.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
+// Build the application.
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "webapp_Aseos v1"));
 }
 
 app.UseHttpsRedirection();
