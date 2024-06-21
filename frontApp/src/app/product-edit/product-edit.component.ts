0eb3a57e0f5c5da3d216a983daf2fadee4a62726
= <=>
2024-06-21 15:18:03 -0700

    Product-Edit service added and implemented

--- frontApp/src/app/product-edit/product-edit.component.ts
+++ frontApp/src/app/product-edit/product-edit.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductEditService } from './product-edit.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ChangeDetectorRef } from '@angular/core';  
 
 @Component({
   selector: 'app-product-edit',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './product-edit.component.html',
   styleUrl: './product-edit.component.css'
 })
-export class ProductEditComponent {
 
+export class ProductEditComponent implements OnInit{
+  data: any = [];
+  numberValue: number = 1;
+
+  idValue: number = 1;
+  nameValue: String = '';
+  descriptionValue: String = '';
+  priceValue: number = 0.0;
+  stockValue: number = 0;
+
+  constructor(private productEditService: ProductEditService, private cd: ChangeDetectorRef){}
+  ngOnInit() {
+    this.productEditService.getProduct(this.numberValue).subscribe(data => {
+      this.data = data;
+    });
+  }
+
+  sendNumber() {
+    if (this.numberValue) {
+      this.productEditService.getProduct(this.numberValue).subscribe(data => {
+        this.data = data;
+      });
+    }
+  }
+
+  updateProduct() {
+    // Crea un objeto con los valores de los inputs  
+    let jsonData = {
+      'id': this.numberValue,
+      'nombre': this.nameValue,
+      'descripcion': this.descriptionValue,
+      'precio': this.priceValue,
+      'stock': this.stockValue
+    };
+
+    this.productEditService.updateProduct(this.numberValue, jsonData);
+    this.sendNumber();
+  }
 }
