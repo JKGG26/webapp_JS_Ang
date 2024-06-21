e21612e59e1486a707c485d9dd8e85d3c9293414
= <=>
2024-06-21 11:27:12 -0700

    Service for Product-Detail added and integrated

--- frontApp/src/app/product-detail/product-detail.component.ts
+++ frontApp/src/app/product-detail/product-detail.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductDetailService } from './product-detail.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
-export class ProductDetailComponent {
 
+export class ProductDetailComponent implements OnInit{
+  data: any = [];
+  numberValue: number = 1;
+
+  constructor(private productDetailService: ProductDetailService){}
+  ngOnInit() {
+    this.productDetailService.getProduct(this.numberValue).subscribe(data => {
+      this.data = data;
+    });
+  }
+
+  sendNumber() {
+    if (this.numberValue) {
+      this.productDetailService.getProduct(this.numberValue).subscribe(data => {
+        this.data = data;
+      });
+    }
+  }
 }
---
+++ frontApp/src/app/product-detail/product-detail.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductDetailService {
+  private apiUrl = 'http://localhost:5235/api';
+
+  constructor(private http: HttpClient) {}
+
+  getProduct(number: number): Observable<any[]>{
+    return this.http.get<any[]>(`${this.apiUrl}/Productos/${number}`).pipe(  
+      map(response => Array.isArray(response) ? response : [response]),  
+      catchError(this.handleError)  
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // Error del lado del cliente
+      console.error('Ocurrió un error:', error.error.message);
+    } else {
+      // El servidor devolvió un código de error
+      console.error(`Código de error ${error.status}, ` + `mensaje: ${error.message}`);
+    }
+    // Devuelve un observable con un mensaje de error
+    return throwError('Algo salió mal; inténtalo de nuevo más tarde.');
+  }
+}
