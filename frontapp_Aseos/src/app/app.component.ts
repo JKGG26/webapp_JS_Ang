098973368b93fb8157d92adf31451438ebda8f8d
JhormanGonzalez <jgonzalez202303@gmail.com>
2024-06-28 12:23:41 -0500

    Routin packages, Header, Footer, Home, Products components imported

--- frontapp_Aseos/src/app/app.component.ts
+++ frontapp_Aseos/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+    ProductsPageComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
