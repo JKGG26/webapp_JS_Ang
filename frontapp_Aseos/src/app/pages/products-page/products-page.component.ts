09f6e52e48453aeab73b61af640a3d4339ebf37b
JhormanGonzalez <jgonzalez202303@gmail.com>
2024-06-28 19:41:43 -0500

    product-page and layout updated to show table and fixed components

--- frontapp_Aseos/src/app/pages/products-page/products-page.component.ts
+++ frontapp_Aseos/src/app/pages/products-page/products-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ProductsTableComponent } from './components/products-table/products-table.component';
 
 @Component({
   selector: 'app-products-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ProductsTableComponent
+  ],
   templateUrl: './products-page.component.html',
   styleUrl: './products-page.component.css'
 })
